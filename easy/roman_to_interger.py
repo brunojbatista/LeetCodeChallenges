d83b9b1b415c2eac44313a46c303db2c48aa283b
Bruno Batista <bjnb.projetos@gmail.com>
2025-05-26 11:01:29 -0300

    feat: create the conversion of Roman numerals to integers

---
+++ easy/roman_to_interger.py
@@ -0,0 +1,33 @@
+class Solution:
+    ROMAN_NUMERALS = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    def romanToInt(self, s: str) -> int:
+        totalLetters = len(s)
+        lastNumber = 0
+        value = 0
+        """
+            A cada iteração, é lido a ultima letra e extraido o valor dele,
+            caso o valor atual seja maior ou igual ao anterior soma-se ao 
+            valor final, caso contrário subtrai-se do valor final.
+        """
+        for i in range(totalLetters - 1, -1, -1):
+            currentValue = Solution.ROMAN_NUMERALS[s[i]]
+            if currentValue >= lastNumber:
+                value += currentValue
+            else:
+                value -= currentValue
+            lastNumber = currentValue
+        return value
+
+print(Solution().romanToInt("III")) # 3
+
+print(Solution().romanToInt("LVIII")) # 58
+
+print(Solution().romanToInt("MCMXCIV")) # 1994
